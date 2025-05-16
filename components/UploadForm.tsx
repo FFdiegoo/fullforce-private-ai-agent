fe33d9b72f4d04bf58f883de7e568af7683bcd63
FFdiegoo <diego@full-force.ai>
2025-05-16 13:26:40 +0200

    zelfde fix, ander zittetje

--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -1,5 +1,5 @@
 // components/UploadForm.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
 
 export default function UploadForm() {
