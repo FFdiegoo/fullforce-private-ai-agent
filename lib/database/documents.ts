28cecf2d998abbc74bac98cf558b7274e1fe3068
FFdiegoo <diego@full-force.ai>
2025-07-30 21:27:35 +0200

    Fix Import Error voor DocumentChunkWithDocument

--- lib/database/documents.ts
+++ lib/database/documents.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { Document, DocumentChunk, EmbeddingStatus, DocumentStatus } from '../types/database';
+import { Document, DocumentChunk, DocumentChunkWithDocument, EmbeddingStatus, DocumentStatus } from '../types/database';
 
 const prisma = new PrismaClient();
 
